493cb8416d3b9c84f8479d50d87cae2ed2a00758
Ayokunle Odutayo <40292574+odunet@users.noreply.github.com>
2022-01-17 12:22:45 +1100

    Import dependencies in layout.jsx

--- website/src/components/layout.js
+++ website/src/components/layout.js
@@ -1,7 +1,40 @@
 import * as React from "react"
 import { Link } from "gatsby"
 
+import rc from '@stripe/react-stripe-js'
+import PropTypes from 'prop-types'
+import loadable from '@loadable/component'
+import rehypeRaw from 'rehype-raw' // plugin to parse the tree again (and raw nodes). Improper use of rehype-raw can open you up to a cross-site scripting (XSS) attack. Either do not combine this plugin with user content or use rehype-sanitize.
+import rehypeSanitize from 'rehype-sanitize' 
+import { loadStripe } from '@stripe/stripe-js'
+import queryString from 'query-string'
+import 'rc-slider/assets/index.css'
+import { Doughnut } from 'react-chartjs-2'
+import Slider from 'react-slick'
+import 'react-datepicker/dist/react-datepicker.css'
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
+import { useInView } from 'react-intersection-observer'
+import ReactMapGL, { Marker, NavigationControl } from 'react-map-gl'
+
 const Layout = ({ location, title, children }) => {
+  /**
+   * Dependencies used in project
+   */
+  const a = rc
+  const b = PropTypes
+  const c = loadable
+  const d = rehypeRaw
+  const e = rehypeSanitize
+  const f = loadStripe
+  const g = queryString
+  const h = Doughnut
+  const i = useInView
+  const j = ReactMapGL
+  const k = Marker
+  const l = NavigationControl
+  const m = Slider
+  
   const rootPath = `${__PATH_PREFIX__}/`
   const isRootPath = location.pathname === rootPath
   let header
